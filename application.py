dae0419b2aec7b46292d14255fe1f713ca322e41
Nevette <agnieszka.dorota.dzieciol@gmail.com>
2018-03-04 21:50:38 +0100

    Added brightness edition, checking slider value

--- application.py
+++ application.py
@@ -1,17 +1,21 @@
 from tkinter import *
 from tkinter import filedialog
 from PIL import ImageTk, Image
+from enum import Enum
 import PIL.Image
+from copy import copy
 
 
 class MainFrame:
-    second_img = None
+    source_image = None
+    chosen_operation = None
+    using_alfa = False
 
     def __init__(self, frame):
         self.frame = frame
         frame.title("Image Procesor")
         frame.resizable(width=TRUE, height=TRUE)
-        frame.geometry('{}x{}'.format(600, 600))
+        frame.geometry('{}x{}'.format(600, 400))
 
         menu = Menu(app)
         app.config(menu=menu)
@@ -23,11 +27,7 @@ class MainFrame:
         operations = Menu(menu)
         menu.add_cascade(label="Operations", menu=operations)
         operations.add_command(label="Greyscale", command=self.convert_to_greyscale)
-        operations.add_command(label="Brightness")
-
-        # second box for image
-        self.frame2 = Frame(frame, bg="green", width=200, height=150)
-        self.frame2.pack(side=LEFT, anchor=N)
+        operations.add_command(label="Brightness", command=self.convert_brightness)
 
         select_field = StringVar(app)
         # initial value
@@ -39,13 +39,16 @@ class MainFrame:
         self.start_button = Button(frame, text="Start")
         self.start_button.pack(side=LEFT, anchor=S)
 
-        w = Scale(frame, from_=0, to=10, orient=HORIZONTAL)
-        w.pack(side=RIGHT, anchor=S)
+        scale = Scale(frame, from_=-1, to=1, orient=HORIZONTAL, resolution=0.01, command=self.slider_value)
+        scale.pack(side=RIGHT, anchor=S)
 
     def choose_file(self):
         # open dialog window
-        first_image = "C:/Users/Agnieszka/Documents/a.png"  # filedialog.askopenfilename()
+        first_image = filedialog.askopenfilename()
         image = PIL.Image.open(first_image)
+
+        if first_image.endswith(".png"):
+            self.using_alfa = True
 
         # set image size
         size = 200, 200
@@ -59,26 +62,72 @@ class MainFrame:
         size2 = 200, 200
         image.thumbnail(size2, Image.ANTIALIAS)
 
-        self.second_img = image
+        self.source_image = image
         self.update_second_image(image)
-
-    def convert_to_greyscale(self):
-        image_data = self.second_img.load()
-
-        # TODO: add checking image type
-        for x in range(self.second_img.width):
-            for y in range(self.second_img.height):
-                rgb = image_data[x, y]
-                grey = int((rgb[0] + rgb[1] + rgb[2])/3)
-                image_data[x, y] = (grey, grey, grey, rgb[3])  # if jpg: image_data[x, y] = (grey, grey, grey)
-
-        self.update_second_image(self.second_img)
 
     def update_second_image(self, image_data):
         second_photo = ImageTk.PhotoImage(image_data)
         img2 = Label(self.frame, image=second_photo)
         img2.image = second_photo
         img2.place(x=300, y=0)
+
+    def convert_to_greyscale(self):
+        self.chosen_operation = Operation.GREYSCALE
+        image_data = self.source_image.load()
+
+        if self.using_alfa:
+            for x in range(self.source_image.width):
+                for y in range(self.source_image.height):
+                    rgb = image_data[x, y]
+                    grey = int((rgb[0] + rgb[1] + rgb[2])/3)
+                    image_data[x, y] = (grey, grey, grey, rgb[3])
+        else:
+            for x in range(self.source_image.width):
+                for y in range(self.source_image.height):
+                    rgb = image_data[x, y]
+                    grey = int((rgb[0] + rgb[1] + rgb[2])/3)
+                    image_data[x, y] = (grey, grey, grey)
+
+        self.update_second_image(self.source_image)
+
+    def convert_brightness(self):
+        self.chosen_operation = Operation.BRIGHTNESS
+
+    def adjust_brightness(self, slider_value):
+        image = copy(self.source_image)
+        image_data = image.load()
+
+        if self.using_alfa:
+            for x in range(image.width):
+                for y in range(image.height):
+                    modifier = int(255 * float(slider_value))
+                    rgb = image_data[x, y]
+                    r = rgb[0] + modifier
+                    g = rgb[1] + modifier
+                    b = rgb[2] + modifier
+
+                    image_data[x, y] = (r, g, b, rgb[3])
+        else:
+            for x in range(image.width):
+                for y in range(image.height):
+                    modifier = int(255 * float(slider_value))
+                    rgb = image_data[x, y]
+                    r = rgb[0] + modifier
+                    g = rgb[1] + modifier
+                    b = rgb[2] + modifier
+
+                    image_data[x, y] = (r, g, b)
+
+        self.update_second_image(image)
+
+    def slider_value(self, value):
+        if self.chosen_operation == Operation.BRIGHTNESS:
+            self.adjust_brightness(value)
+
+
+class Operation(Enum):
+    GREYSCALE = 1
+    BRIGHTNESS = 2
 
 
 app = Tk()
